33952b4945a9ac11cf74a18295ee09646589a2fa
phatdroid94 <phatdroid94@gmail.com>
2019-10-04 18:42:33 +0700

    init api

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+    // 1 : Khai bao bien
+    // 2 : Toan tu
+    // 3 : Cau dieu kien
+    // 4 : Vong lap
+    // 5 : Object method
+    // 6 : Array method
+    // 7 : OOP
+    // 8 : Xu ly file 
+
+    $a = "Hello";
+    $a = "Xin chao";
+    
+    echo $a;
+
+
+?>
