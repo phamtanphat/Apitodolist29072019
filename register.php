53a20b8271a9c8df2c32182d7145f4a0e0da8a29
phatdroid94 <phatdroid94@gmail.com>
2019-10-07 19:08:20 +0700

    finish register

--- register.php
+++ register.php
@@ -7,6 +7,23 @@
 
     $insertAccount = "INSERT INTO user VALUES (null , '$username' , '$username')";
 
-    echo $username;
+    $data = mysqli_query($con,$insertAccount);
+
+// 1 : UserResponse -> class cho viec tra ve
+    class UserResponse{
+        function __construct($success , $message){
+            $this->success=$success;
+            $this->message=$message;
+        }
+    }
+    // { "success " : true , "message" : "Thanh cong"}
+    // { "success " : false , "message" : "That bai"}
+
+    if(mysqli_error($con)){
+        echo json_encode(new UserResponse(false,"That bai"));
+    }else{
+        echo  json_encode(new UserResponse(true,"Thanh cong"));
+    }
+   
 
 ?>
