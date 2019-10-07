3f4373153c79315f2ce2727883b7f259b606a2f1
phatdroid94 <phatdroid94@gmail.com>
2019-10-07 19:09:27 +0700

    fix field password

--- register.php
+++ register.php
@@ -5,7 +5,7 @@
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $insertAccount = "INSERT INTO user VALUES (null , '$username' , '$username')";
+    $insertAccount = "INSERT INTO user VALUES (null , '$username' , '$password')";
 
     $data = mysqli_query($con,$insertAccount);
 
